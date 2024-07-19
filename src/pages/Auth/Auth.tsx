f2da9a22a798dd92fb581ae91e07bc178a7ea86b
RamdlanFaqih <ramfaqih@gmail.com>
2024-07-19 22:44:23 +0700

    initial setup

---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+      Hello From Auth
+    </div>
+  )
+}
+
+export default Auth
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen bg-black text-white'>
+      Hello From Home !
+    </div>
+  )
+}
+
+export default Home
