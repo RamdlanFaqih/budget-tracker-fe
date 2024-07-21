7e0f1e581f3818158d151e311bb5171409425417
RamdlanFaqih <ramfaqih@gmail.com>
2024-07-21 20:33:06 +0700

    style: initial layout home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className='h-screen bg-black text-white'>
-      Hello From Home !
+    <div className="h-screen">
+      <div className="h-full flex items-center justify-center">
+        <div className="flex flex-col gap-3 text-center">
+        <p className="text-2xl text-dark font-bold">
+          Selamat datang di website budget tracker
+        </p>
+        <Link to={"/login"}>
+          <Button className="w-full">Sign In</Button>
+        </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
